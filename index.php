9354b316afe922b07e3c88e9c3e9a7cdedbac05d
Johanne <johanne_lewis@hotmail.com>
2019-09-09 22:51:33 +0100

    Page markup, add header area

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web App Hub</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div id="header">
+        <div id="logo">
+            <img src="img/logo.png" />
+        </div>
+        <div id="link">
+            <ul>
+                <li><a href="#">Download App</a></li>
+                <li><a href="#">Signup</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
