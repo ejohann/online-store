edb74504c8888c08786fe71e388c167441af7026
Johanne <johanne_lewis@hotmail.com>
2019-09-10 13:24:11 +0100

    Add a category form

--- admin/inc/category.php
+++ admin/inc/category.php
@@ -1,6 +1,15 @@
 <?php if(isset($_GET['view_all_categories'])) : ?>
     <div id="bodyright">
-
+        <h3>Add New Category Here</h3>
+        <form method="POST">
+            <table>
+                <tr>
+                    <td>Enter Category Name: </td>
+                    <td><input type="text" name="category_name"></td>
+                </tr>
+            </table>
+            <center><button name="add_category">Add Category</button></center>
+        </form>
     </div>
 <?php else : ?>
    
