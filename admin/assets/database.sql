4ad151ba07661981bc7c27d93a803f54a231f53b
Johanne <johanne_lewis@hotmail.com>
2019-09-10 14:31:39 +0100

    Sub category category table sql

--- admin/assets/database.sql
+++ admin/assets/database.sql
@@ -4,3 +4,11 @@ CREATE TABLE main_cat(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(100) UNIQUE NOT NULL
 );
+
+CREATE TABLE sub_cat(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    sub_category_name VARCHAR(100) NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY(category_id) REFERENCES main_cat(id),
+    UNIQUE KEY(sub_category_name, category_id)
+);
