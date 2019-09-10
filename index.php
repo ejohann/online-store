f18f98583c1121b46f9b6794163210db4d5981bb
Johanne <johanne_lewis@hotmail.com>
2019-09-10 11:49:51 +0100

    Include template files

--- index.php
+++ index.php
@@ -10,18 +10,9 @@
 <body>
     <?php include_once("./inc/header.php"); ?>
     <?php include_once("./inc/navbar.php"); ?>
-    <div id="bodyleft">
-        <h2>DEALS OF THE DAY</h2>
-        <div id="slider">
-            <img src="https://source.unsplash.com/700x350"" alt="" />
-        </div>
-    </div>
-    <div id="bodyright">
-        <h3>GREAT DEALS</h3>
-    </div>
+    <?php include_once("./inc/bodyleft.php"); ?>
+    <?php include_once("./inc/bodyright.php"); ?>
     <br clear="all" />
-    <div id="footer">
-        <h4>Copyright 2019 &copy; Online Store Developed By Hanne Digital</h4>
-    </div>
+    <?php include_once("./inc/footer.php"); ?>
 </body>
 </html>
