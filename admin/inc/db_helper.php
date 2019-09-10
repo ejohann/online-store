e68ba72d6dc0a062b13316725a0e9d8bc11c9522
Johanne <johanne_lewis@hotmail.com>
2019-09-10 15:03:43 +0100

    Method for returning all categories

--- admin/inc/db_helper.php
+++ admin/inc/db_helper.php
@@ -10,6 +10,15 @@
             $insert_category = null;
             return $id ? $id : false;
         }
+
+        public static function allCategories(){
+            global $database;
+            $select_categories = $database->connection->prepare("SELECT * FROM main_cat");
+            $select_categories->execute();
+            $results = $select_categories->fetch(PDO::FETCH_ASSOC);
+            return $results ? $results : false;
+        }
+    
     }
 
 
