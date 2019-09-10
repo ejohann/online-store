31265bfb510fbed3b0abb1a48bab0196bac6192e
Johanne <johanne_lewis@hotmail.com>
2019-09-10 08:23:35 +0100

    Add a slider section and right sidebar

--- index.php
+++ index.php
@@ -37,5 +37,14 @@
             <li><a href="#">BRANDS</a></li>
         </ul>
     </div>
+    <div id="bodyleft">
+        <h2>DEALS OF THE DAY</h2>
+        <div id="slider">
+            <img src="https://source.unsplash.com/700x350"" alt="" />
+        </div>
+    </div>
+    <div id="bodyright">
+        <h3>GREAT DEALS</h3>
+    </div>
 </body>
 </html>
