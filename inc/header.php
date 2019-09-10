8b94efd1f6f393c0ddebcb317feb3de20adba29a
Johanne <johanne_lewis@hotmail.com>
2019-09-10 11:40:01 +0100

    Header template file with code

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<div id="header">
+    <div id="logo">
+        <img src="img/logo.png" />
+    </div>
+    <div id="link">
+        <ul>
+            <li><a href="#">Download App</a></li>
+            <li><a href="#">Signup</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </div>
+    <div id="search">
+        <form method="GET" enctype="multipart/form-data">
+            <input type="text" placeholder="search here...">
+            <button id="search-btn">Search</button>
+            <button id="cart-btn">Cart</button>
+        </form>
+    </div> 
+</div>
