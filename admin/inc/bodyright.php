6736bf8550bc118295675d0d4c50240e6586c33a
Johanne <johanne_lewis@hotmail.com>
2019-09-10 14:50:26 +0100

    Modified logic

--- admin/inc/bodyright.php
+++ admin/inc/bodyright.php
@@ -1,4 +1,4 @@
-<?php if(isset($_GET['view_all_categories'])) : ?>
+<?php if(isset($_GET['view_all_categories']) || isset($_GET['view_all_sub_categories']) ) : ?>
 
 <?php else : ?>
     <div id="bodyright">
