f698a04971a79c830ef0314b2cbc3c10d99cde45
Johanne <johanne_lewis@hotmail.com>
2019-09-10 14:42:55 +0100

    View sub categories

--- admin/inc/bodyleft.php
+++ admin/inc/bodyleft.php
@@ -3,13 +3,16 @@
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="index.php?view_all_categories">View All Categories</a></li>
-        <li><a href="#">View All Sub-categories</a></li>
+        <li><a href="index.php?view_all_sub_categories">View All Sub-categories</a></li>
     </ul>
 </div>
 
 <?php 
     if(isset($_GET['view_all_categories'])){
         include_once("category.php");
+    }
+    if(isset($_GET['view_all_sub_categories'])){
+        include_once("sub_category.php");
     }
 
 ?>
