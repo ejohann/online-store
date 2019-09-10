712ffd75b0788ef6fcfa0fbc32b15d47c2562cc6
Johanne <johanne_lewis@hotmail.com>
2019-09-10 12:18:44 +0100

    Add side menu

--- admin/inc/bodyleft.php
+++ admin/inc/bodyleft.php
@@ -1,3 +1,8 @@
 <div id="bodyleft">
     <h3>Content Management</h3>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">View All Categories</a></li>
+        <li><a href="#">View All Sub-categories</a></li>
+    </ul>
 </div>
