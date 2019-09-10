5e86b11f6c8be94d30ec835e154a05ba5edd8feb
Johanne <johanne_lewis@hotmail.com>
2019-09-10 07:41:43 +0100

    Add a navigation bar

--- index.php
+++ index.php
@@ -27,5 +27,15 @@
             </form>
         </div> 
     </div>
+    <div id="navbar">
+        <ul>
+            <li><a href="#">CATEGORIES</a></li>
+            <li><a href="#">BIRTHDAY</a></li>
+            <li><a href="#">GIFTS FOR HIM</a></li>
+            <li><a href="#">FLOWERS</a></li>
+            <li><a href="#">GIFTS FOR HER</a></li>
+            <li><a href="#">BRANDS</a></li>
+        </ul>
+    </div>
 </body>
 </html>
